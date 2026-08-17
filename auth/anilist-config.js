8783cf211b72f3585345bcaf03c6418e66094269
chachinn <140357822+chachinn@users.noreply.github.com>
2026-08-18 00:12:35 +0800

    Add AniList auth configuration

---
+++ auth/anilist-config.js
@@ -0,0 +1,10 @@
+window.TOMO_ANILIST_CONFIG = Object.freeze({
+  clientId: '48822',
+  graphqlUrl: 'https://graphql.anilist.co',
+  authorizeUrl: 'https://anilist.co/api/v2/oauth/authorize',
+  storageKey: 'tomo.anilist.auth.v1',
+  cachedProfileKey: 'tomo.anilist.profile.v1',
+  requestWindowMs: 60000,
+  requestLimit: 25,
+  burstGapMs: 350
+});
